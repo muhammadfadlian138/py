525aafc07b7cd84ab04fec524f890f34bc16906f
muhammadfadlian138 <muhammad.fadlian138@admin.sd.belajar.id>
2026-08-29 08:00:42 +0700

    pertama

---
+++ turtle/aug-13.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.fillcolor ("black");
+
+turtle.forward(90);
+turtle.right(90);
+turtle.forward(90);
+turtle.right(90);
+turtle.forward(90);
+turtle.right(90);
+turtle.forward(90);
+turtle.right(90);
